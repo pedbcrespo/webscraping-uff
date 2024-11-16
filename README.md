ccd1329ce3ae253c70e355dc6e738619504f991d
pedro <pedbcrespo@gmail.com>
2024-11-16 18:21:56 -0300

    ajustando para receber nomes e cpfs

--- README.md
+++ README.md
@@ -6,13 +6,13 @@ Este projeto utiliza **Axios** para fazer as requisições e **Puppeteer** ou **
 
 ## Como Usar
 
-Basta importar a função `execute` do arquivo `main.js`, passando como parâmetro uma lista de CPFs. A função irá retornar os dados coletados do site da UFF relacionados aos CPFs fornecidos.
+Basta importar a função `execute` do arquivo `main.js`, passando como parâmetro uma lista de strings, podendo ser tanto uma lista de nomes quanto de CPFs. A função irá retornar os dados coletados do site da UFF relacionados aos CPFs fornecidos.
 
 ### Exemplo de Código
 
 ```javascript
 import { execute } from './main.js';
 
-const resultados = await execute(['12345678901', '11122233345', '33322211165']);
+const resultados = await execute(['Fulano de Tal', '11122233345']);
 
 console.log(resultados); // Exibe os dados coletados
--- main.js
+++ main.js
@@ -3,11 +3,11 @@ import axios from "axios";
 
 const URL_BASE = 'https://app.uff.br/transparencia';
 
-export const execute = async (cpfList) => {
+export const execute = async (list) => {
     let listData = [];
-    const previousList = await getPreviousData(cpfList);
+    const previousList = await getPreviousData(list);
     if(previousList.some(data => !data)) {
-        console.error("LISTA COM DADOS INVALIDOS", cpfList)
+        console.error("LISTA COM DADOS INVALIDOS", list)
         return [];
     }
     for (const previousData of previousList) {
@@ -32,7 +32,7 @@ const extractData = async (listData, previousData) => {
         const tds = Array.from(document.querySelectorAll('#cadastro-pessoal-ALUNO td'));
         return tds.map(td => td.textContent.trim());
     });
-    const userDetails = {};
+    const userDetails = { Nome: previousData.nome };
     data.forEach((info, i) => {
         if(i % 2 == 0)
             userDetails[info] = null;
@@ -44,8 +44,17 @@ const extractData = async (listData, previousData) => {
     return listData;
 }
 
-const getPreviousData = async (cpfList) => {
-    const previousUserDataPromise = cpfList.map(cpf => axios.get(`${URL_BASE}/busca_cadastro.json?term=${cpf}`));
+const getPreviousData = async (elementList) => {
+
+
+
+    const previousUserDataPromise = elementList.map(elem => {
+        let term = elem || '';
+        if(isNaN(parseFloat(elem))) {
+            term = term.trim().replaceAll(' ', '+');
+        }
+        return axios.get(`${URL_BASE}/busca_cadastro.json?term=${term}`);
+    });
     const previousResolvedPromises = await Promise.all(previousUserDataPromise);
     const previousList = previousResolvedPromises.map(res => PreviousUserData(res.data));
     return previousList;
@@ -56,3 +65,7 @@ const PreviousUserData = (resData) => {
     const { cpf, nome, ididentificacao } = resData[0].pessoa;
     return { cpf, nome, identificacao: ididentificacao };
 };
+
+let res = await execute(['Pedro de Barros Crespo', '14754473701']);
+
+console.log(res);
