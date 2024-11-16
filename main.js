ea2d2a3ee15ff9f2f9128394e06113780c3cc7b9
pedro <pedbcrespo@gmail.com>
2024-11-16 11:17:35 -0300

    implementando

--- main.js
+++ main.js
@@ -1,11 +1,32 @@
-import { Builder, Browser } from 'selenium-webdriver';
+import { Builder, Browser, By } from 'selenium-webdriver';
+import axios from 'axios';
+
+const URL_BASE = 'https://app.uff.br/transparencia/busca_cadastro';
 
 const driver = await new Builder()
     .forBrowser(Browser.CHROME)
     .build();
 
-const execute = async (dataList) => {
-    await driver.get('https://google.com');
+
+export const execute = async (cpfList) => {
+    try {
+        await driver.get(URL_BASE);
+        for(const cpf of cpfList) {
+            const inputElement = await driver.findElement(By.xpath('//*[@id="busca"]'));
+            inputElement.sendKeys(cpf);
+        }
+    }
+    catch {
+        console.error(`ERRO AO ACESSAR A URL: '${URL_BASE}'`)
+    }
 }
 
+
+
+const sleep = (ms) => {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+
 execute([]);
+
