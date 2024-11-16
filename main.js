242ebfc24af70b6759824a380f278f5c214d5e1b
pedro <pedbcrespo@gmail.com>
2024-11-16 15:33:47 -0300

    corrigindo erros

--- main.js
+++ main.js
@@ -9,29 +9,35 @@ const PreviousUserData = (resData) => {
 };
 
 export const execute = async (cpfList) => {
+    let listData = [];
     const previousList = await getPreviousData(cpfList);
     for(const previousData of previousList) {
         const { data } = await getDetailsData(previousData);
-        console.log(data);
-        // const html = cheerio.load(data);
-        // console.log("LEU O HTML");
+        listData = extractData(listData, data);
     }
+    return listData
+}
+
+const extractData = (listData, data) => {
+    const html = cheerio.load(data);
+    console.log("LEU O HTML");
+    return listData;
 }
 
 const getDetailsData = async (previousData) => {
-    const detailsUrl = `${URL_BASE}?cpf=${previousData.cpf}&ididentificacao=${previousData.identificacao}&tipo=`;
-    console.log('url', detailsUrl);
-    return await axios.get(detailsUrl);
+    const detailsUrl = `https://app.uff.br/transparencia/busca_cadastro_pessoa?cpf=${previousData.cpf}&ididentificacao=${previousData.identificacao}&tipo=`;
+    const response = await axios.get(detailsUrl);
+    console.log(response);
+    return response;
 }
 
 const getPreviousData = async (cpfList) => {
     const previousUserDataPromise = cpfList.map(cpf => axios.get(`${URL_BASE}.json?term=${cpf}`));
     const previousResolvedPromises = await Promise.all(previousUserDataPromise);
     const previousList = previousResolvedPromises.map(res => PreviousUserData(res.data));
-    console.log("PREVIOUS DATA");
-    console.log(previousList);
-    console.log("==================");
     return previousList;
 }
+
+
 
 execute(['14754473701']);
