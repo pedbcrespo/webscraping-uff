a0e50c40dcf8bc742d59ad0e430aef03a9eb4ec0
pedro <pedbcrespo@gmail.com>
2024-11-16 15:26:39 -0300

    organizando codigo em partes

--- main.js
+++ main.js
@@ -9,20 +9,29 @@ const PreviousUserData = (resData) => {
 };
 
 export const execute = async (cpfList) => {
+    const previousList = await getPreviousData(cpfList);
+    for(const previousData of previousList) {
+        const { data } = await getDetailsData(previousData);
+        console.log(data);
+        // const html = cheerio.load(data);
+        // console.log("LEU O HTML");
+    }
+}
+
+const getDetailsData = async (previousData) => {
+    const detailsUrl = `${URL_BASE}?cpf=${previousData.cpf}&ididentificacao=${previousData.identificacao}&tipo=`;
+    console.log('url', detailsUrl);
+    return await axios.get(detailsUrl);
+}
+
+const getPreviousData = async (cpfList) => {
     const previousUserDataPromise = cpfList.map(cpf => axios.get(`${URL_BASE}.json?term=${cpf}`));
     const previousResolvedPromises = await Promise.all(previousUserDataPromise);
     const previousList = previousResolvedPromises.map(res => PreviousUserData(res.data));
     console.log("PREVIOUS DATA");
     console.log(previousList);
     console.log("==================");
-    for(const previousData of previousList) {
-        const detailsUrl = `${URL_BASE}?cpf=${previousData.cpf}&ididentificacao=${previousData.identificacao}&tipo=`;
-        const response  = await axios.get(detailsUrl);
-        const html = cheerio.load(response.data);
-        console.log("HTML");
-        console.log(html);
-        console.log("==================");
-    }
+    return previousList;
 }
 
 execute(['14754473701']);
